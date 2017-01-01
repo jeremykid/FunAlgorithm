4d980957bef46d3d58cd2e30c44cf3f8d1f75fa1
weijiesun <weijie2@ualberta.ca>
2017-01-01 12:35:49 -0700

    Solve the Hankson

--- Hankson/Hankson.py
+++ Hankson/Hankson.py
@@ -17,7 +17,22 @@ def findFactor(b0):
 	if temp != 1:
 		returnArray.append(temp)
 	return returnArray
-	
+
+def findDivision(value):
+	valueSqrt = math.sqrt(value)
+	i = 2
+	returnArray = [value,1]
+	while i<valueSqrt:
+		if value%i == 0:
+			returnArray.append(i)
+			returnArray.append(value/i)
+		i += 1
+	return returnArray
+
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
 
 def main():
 	times = int(input(""))
@@ -26,7 +41,18 @@ def main():
 		tempArray = temp.split(" ")
 		
 		a0,a1,b0,b1 = int(tempArray[0]),int(tempArray[1]),int(tempArray[2]),int(tempArray[3])
+		returnArray = findFactor(b0)
+		resultFactor = b1/b0
+		newb0 = b0
+		gcdB = 1
+		for i in findFactor(resultFactor):
+			resultPoints = returnArray.count(i)
+			gcdB *= i**resultPoints
+			newb0= newb0/(i**resultPoints)
 
-
+		returnDevision = findDivision(newb0)
+		returnArray = [i*gcdB*resultFactor for i in returnDevision]
+		returnArray = [i for i in returnArray if gcd(i,a0)==a1]
+		print len(returnArray)
 
 main()
