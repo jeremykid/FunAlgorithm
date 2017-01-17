70208be17c190b8741ef9d8266e8b70bed6141f9
weijiesun <weijie2@ualberta.ca>
2017-01-16 18:28:58 -0700

    Finished the algorithm

--- arrangeTasks/arrangeTasks.py
+++ arrangeTasks/arrangeTasks.py
@@ -1,7 +1,14 @@
 def minmumInArray(newArray,originalArray):
-    newArray[0] = newArray[0]+originalArray[0]
-    minimumValue = newArray[0]
-    i = 1
+    sumArray = [0]*len(newArray)
+    for index in range(len(newArray)):
+        sumArray[index] = newArray[index] + originalArray[index]
+
+    minimumValue = min(sumArray)
+    minimumIndex = sumArray.index(minimumValue)
+    newArray[minimumIndex] = minimumValue
+
+    minimumValue = newArray[minimumIndex]
+    i = minimumIndex+1
     while (i<len(newArray) and newArray[i]<minimumValue):
         newArray[i],newArray[i-1] = newArray[i-1],newArray[i]
         originalArray[i],originalArray[i-1] = originalArray[i-1],originalArray[i]
@@ -19,7 +26,6 @@ def main():
     newArray = [0]*len(nArray)
     while (m>0):
         newArray,nArray = minmumInArray(newArray,nArray)
-        print (newArray,nArray)
         # 6,0
         # 6,8
         # 12,8
