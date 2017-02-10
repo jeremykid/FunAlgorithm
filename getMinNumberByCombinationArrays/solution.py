82c9651f397a3a55aa0dec529b92ac8ecc3d3951
weijiesun <weijie2@ualberta.ca>
2017-02-10 10:01:00 -0700

    Add two function about combination

--- getMinNumberByCombinationArrays/solution.py
+++ getMinNumberByCombinationArrays/solution.py
@@ -1,10 +1,36 @@
-def getAllCombination(inputList, returnList, newItem):
-        if len(inputList) == 0:
-                returnList.append(newItem)
-                return returnList
-        else:
-                for i in inputList:
-                        newItem = newItem+i
-                        inputList.remove(i)
-                        returnList = getAllCombination(inputList,returnList,newItem)
+def getAllStringCombination(inputList, returnList, newItem):
+    if len(inputList) == 0:
+        returnList.append(newItem)
         return returnList
+    else:
+        for i in inputList:
+            tempList = list(inputList)
+            tempList.remove(i)
+            returnList = getAllStringCombination(tempList,returnList,newItem+i)
+
+    return returnList
+
+def getAllListCombination(inputList, returnList, newItem):
+    if len(inputList) == 0:
+        returnList.append(newItem)
+        return returnList
+    else:
+        for i in inputList:
+            tempList = list(inputList)
+            tempList.remove(i)
+            tempItem = list(newItem)
+            newItem.append(i)
+            returnList = getAllListCombination(tempList,returnList,newItem)
+            newItem = list(tempItem)
+    return returnList
+
+def main():
+    # inputList = input(":")
+    inputList = ["1","2","3"]
+    returnList = getAllStringCombination(inputList,[],"")
+    print returnList
+    print inputList
+    returnList = getAllListCombination(inputList, [],[])
+    print returnList
+
+main()
