4752464ac9b44ebe29de65322b1c3e056ea91806
weijiesun <weijie2@ualberta.ca>
2017-03-20 23:45:35 -0600

    Add undirected graph

---
+++ python_practice/graph/undirectedGraph.py
@@ -0,0 +1,10 @@
+
+class undirectedGraph(object):
+	def __init__(self, degrees):
+		self.degrees = degrees
+		self.adjacent_matrix = []
+		for i in range(degrees):
+			self.adjacent_matrix.append([0]*degrees)
+
+	def __str__(self):
+		return self.adjacent_matrix
---
+++ python_practice/graph/undirectedGraphTest.py
@@ -0,0 +1,7 @@
+from undirectedGraph import undirectedGraph
+
+def main():
+	test1 = undirectedGraph()
+	print test1
+
+main()
