4a6c2c8afd7f83487eea7dec827c8653ce1f52ea
weijie2 <jeremykid1992@gmail.com>
2017-03-22 16:48:08 -0600

    Add three more tests

--- python_practice/graph/undirectedGraphTest.py
+++ python_practice/graph/undirectedGraphTest.py
@@ -17,10 +17,27 @@ def main():
 	print test1
 	
 	#todo test depthFirstSearch
+	# 1->2
+	#    2->5
+	# 1->3
+	#    3->4
+	result = test1.depthFirstSearch(1)
+	print "=== Depth First Search ==="
+	print result
 	
 	#todo test breathFirstSearch
+	# 1->2
+	# 1->3
+	#    2->5
+	#    3->4
+	result = test1.breathFirstSearch(1)
+	print "=== Breath First Search ==="
+	print result
 	
 	#todo test get all edges
+	result = test1.getAllEdges(1)
+	print "=== Get All Edges ==="
+	print result
 	
 	#todo test is tree (is it a cycle)
 main()
