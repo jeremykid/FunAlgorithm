9ea141411080d6a75568364d449182121658e0aa
weijie2 <jeremykid1992@gmail.com>
2017-04-05 14:30:46 -0600

    Add sort algorithm

---
+++ python_practice/sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(input_list):
+  input_list_length = len(input_list)
+  for passed in range(input_list_length):
+    for index in range(passed, input_list_length-1):
+      if (input_list[index] > input_list[index+1])
+        input_list[index], input_list[index+1] =  input_list[index+1], input_list[index]
+      
+  return input_list
+  
+def insert_sort(input_list):
+
+
+  return input_list
