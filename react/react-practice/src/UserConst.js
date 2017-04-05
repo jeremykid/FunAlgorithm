d2ca7d365638e2a6ba8e46516cfe344d9a5c3e76
weijie2 <jeremykid1992@gmail.com>
2017-04-05 09:46:43 -0600

    Add user const.

---
+++ react/react-practice/src/UserConst.js
@@ -0,0 +1,19 @@
+const user_application = [
+  
+  applicationId = "",
+  applicationName = "",
+  isBookMark = 0,
+  userId = "",
+  schoolId = "",
+  facultyId = "",
+  programId = "",
+  applicationState = "",
+  
+  
+];
+
+const user_application_object = [
+ {
+  
+ }, 
+];
