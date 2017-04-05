09bb9a917fefd44881ceb46f73f494603797bbbf
weijie2 <jeremykid1992@gmail.com>
2017-04-05 17:05:08 -0600

    Add numpy exercise todo

---
+++ python_practice/numpy_exercise1.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# init
+matrix_a = np.array([2,3,4])
+print matrix_a
+
+# shape
+print matrix_a.shape
+
+# reshape
+
+# ndim
+print matrix_a.ndim
+
+# dtype
+print matrix_a.dtype
+
+# itemsize
+print matrix_a.itemsize
+
+# size
+print matrix_a.size
+
