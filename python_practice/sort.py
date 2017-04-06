86cdf0e298565583eb5d4acae249943f59f9e5cf
weijie2 <jeremykid1992@gmail.com>
2017-04-06 15:48:31 -0600

    Add insert sort

--- python_practice/sort.py
+++ python_practice/sort.py
@@ -8,6 +8,20 @@ def bubble_sort(input_list):
   return input_list
   
 def insert_sort(input_list):
-
-
+  input_list_length = len(input_list)
+  for i in range(1,input_list_length):
+    result_list = insert_sort_single_sort(input_list[:i], input_list[i])
+    if (i != input_list_length):
+      input_list = result_list + input_list[i+1:] 
   return input_list
+
+def insert_sort_single_sort(input_list, new_element):
+  is_insert = false
+  for i in range(len(input_list)):
+    if input_list[i] > new_element:
+      input_list.insert(i,new_element)
+      is_insert = true
+  if is_isert == false:
+    input_list.append(new_element)
+  return input_list
+
