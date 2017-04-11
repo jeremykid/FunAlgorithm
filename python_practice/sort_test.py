e8259c087400276730a361971c5643e1b2c9e444
weijie2 <jeremykid1992@gmail.com>
2017-04-11 12:39:42 -0600

    Add more unit tests

--- python_practice/sort_test.py
+++ python_practice/sort_test.py
@@ -10,4 +10,21 @@ def main():
   
   print sort.quick_sort(test1_list)
   
+  test2_list = [9,7,5,4,1]
+  print sort.bubble_sort(test2_list)
+  
+  print sort.insert_sort(test2_list)
+  
+  print sort.merge_sort(test2_list)
+  
+  print sort.quick_sort(test2_list)
+  
+  test3_list = [11,15,86,2,4]
+  print sort.bubble_sort(test3_list)
+  
+  print sort.insert_sort(test3_list)
+  
+  print sort.merge_sort(test3_list)
+  
+  print sort.quick_sort(test3_list)
 main()
