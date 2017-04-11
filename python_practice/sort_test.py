24494273124aa45d8a26970418861addf90baca5
weijie2 <jeremykid1992@gmail.com>
2017-04-11 11:53:40 -0600

    Add tests for sort

---
+++ python_practice/sort_test.py
@@ -0,0 +1,13 @@
+import sort 
+
+def main():
+  test1_list = [1,3,5,7,9]
+  print sort.bubble_sort(test1_list)
+  
+  print sort.insert_sort(test1_list)
+  
+  print sort.merge_sort(test1_list)
+  
+  print sort.quick_sort(test1_list)
+  
+main()
