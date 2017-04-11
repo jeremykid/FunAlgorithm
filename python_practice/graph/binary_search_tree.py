d6bfad45f27d3be894f35cbd0fa640c1be4a167a
weijie2 <jeremykid1992@gmail.com>
2017-04-11 13:04:56 -0600

    Add insert_node and get_node_count

--- python_practice/graph/binary_search_tree.py
+++ python_practice/graph/binary_search_tree.py
@@ -11,4 +11,26 @@ class binary_search_tree(object):
     print self.right_node
 
   def insert_node(self, insert_value):
-    
+    if insert_value == self.value:
+      return false
+    if insert_value > self.value:
+      if self.right_node == None:
+        self.right_node = binary_search_tree(insert_value)
+      else:
+        self.right_node.insert_node(insert_value)
+    else:
+      if self.left_node == None:
+        self.left_node = binary_search_tree(insert_value)
+      else:
+        self.left_node.insert_node(insert_value)
+    return true
+  
+  def get_node_count(self):
+    return_count = 1
+    if self.right_node != None:
+      return_count += self.right_node.get_node_count()
+    if self.left_node != None:
+      return_count += self.left_node.get_node_count()
+    return return_count
+  
+  
