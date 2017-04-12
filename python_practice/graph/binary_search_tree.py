507f90417338ffd3221723544bb68c680fbca1b8
weijie2 <jeremykid1992@gmail.com>
2017-04-12 13:29:21 -0600

    Add is in tree and fix is_in_tree

--- python_practice/graph/binary_search_tree.py
+++ python_practice/graph/binary_search_tree.py
@@ -56,17 +56,26 @@ class binary_search_tree(object):
       return self.right_node.get_max()
     
   def is_binary_search_tree(self):
-    result = false
+    result = False
     if self.left_node.value < self.left_node.get_max():
       return result
     else:
-      result = (result and self.left_node.is_binary_search_tree())
+      return self.left_node.is_binary_search_tree()
     if self.right_node.value > self.right_node.get_min():
       return result
     else:
-      result = (result and self.left_node.is_binary_search_tree())
-    return result
+      return self.left_node.is_binary_search_tree()
   
-  def is_in_tree(self):
-    #todo
-    return true
+  def is_in_tree(self, val):
+    if self.value == val:
+      return True
+    elif self.value > val:
+      if self.left_node == None:
+        return False
+      else:
+        return self.left_node.is_in_tree(val))
+    else:
+      if self.right_node == None:
+        return False
+      else:
+        return self.right_node.is_in_tree(val)
