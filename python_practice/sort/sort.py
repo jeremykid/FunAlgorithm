1e9ac1cc3ff2ed41b2285a1f3a90c7191fca8345
weijiesun <weijie2@ualberta.ca>
2017-04-16 22:01:03 -0600

    Fix the merge sort

--- python_practice/sort/sort.py
+++ python_practice/sort/sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(input_list):
   input_list_length = len(input_list)
-  for passed in range(input_list_length):
-    for index in range(passed, input_list_length-1):
-      if (input_list[index] > input_list[index+1])
+  for passed in range(input_list_length-1):
+    for index in range(input_list_length-1):
+      if (input_list[index] > input_list[index+1]):
         input_list[index], input_list[index+1] =  input_list[index+1], input_list[index]
-      
+      # print input_list
   return input_list
   
 def insert_sort(input_list):
@@ -16,30 +16,55 @@ def insert_sort(input_list):
   return input_list
 
 def insert_sort_single_sort(input_list, new_element):
-  is_insert = false
+  is_insert = False
   for i in range(len(input_list)):
     if input_list[i] > new_element:
       input_list.insert(i,new_element)
-      is_insert = true
-  if is_isert == false:
+      is_insert = True
+  if is_insert == False:
     input_list.append(new_element)
   return input_list
 
+def msort(x):
+    result = []
+    if len(x) < 2:
+        return x
+    mid = int(len(x)/2)
+    y = msort(x[:mid])
+    z = msort(x[mid:])
+    while (len(y) > 0) or (len(z) > 0):
+        if len(y) > 0 and len(z) > 0:
+            if y[0] > z[0]:
+                result.append(z[0])
+                z.pop(0)
+            else:
+                result.append(y[0])
+                y.pop(0)
+        elif len(z) > 0:
+            for i in z:
+                result.append(i)
+                z.pop(0)
+        else:
+            for i in y:
+                result.append(i)
+                y.pop(0)
+    return result
+
 def merge_sort(input_list):
   input_list_length = len(input_list)
+  result_list = input_list
   if (input_list_length > 1):
     mid = input_list_length//2
     left_list = input_list[:mid]
     right_list = input_list[mid:]
-    
+
     left_list = merge_sort(left_list)
     right_list = merge_sort(right_list)
     
     left_index = 0
     right_index = 0
     index = 0
-    
-    result_list = []
+    result_list = [0]*input_list_length
     while left_index < len(left_list) and right_index < len(right_list):
       if left_list[left_index] < right_list[right_index]:
         result_list[index] = left_list[left_index]  
@@ -48,13 +73,13 @@ def merge_sort(input_list):
         result_list[index] = right_list[right_index]  
         right_index = right_index+1
       index += 1 
-        
+
     while left_index < len(left_list):
       result_list[index] = left_list[left_index]  
       left_index = left_index+1
       index += 1 
 
-    while right_list < len(right_list):
+    while right_index < len(right_list):
       result_list[index] = right_list[right_index]  
       right_index = right_index+1
       index += 1 
@@ -67,7 +92,7 @@ def quick_sort_recursion(input_list):
   input_list_length = len(input_list)
   mark = input_list[0]
   left_i = 1
-  right_i = input_list_length
+  right_i = input_list_length-1
   while right_i > left_i:
     if input_list[left_i] > mark:
       if input_list[right_i] < mark:
@@ -78,7 +103,10 @@ def quick_sort_recursion(input_list):
         right_i -= 1
     else:
       left_i += 1
-  input_list = quick_sort_recursion[1:right_i] + [mark] + quick_sort_recursion[right_i+1:]
-  return input_list
+  print right_i
+  print input_list[1:right_i]
+
+  # input_list = quick_sort_recursion([1:right_i]) + [mark] + quick_sort_recursion([right_i+1:])
+  # return input_list
         
   
--- python_practice/sort/sort_test.py
+++ python_practice/sort/sort_test.py
@@ -1,30 +1,30 @@
-import sort 
+from sort import * 
 
 def main():
   test1_list = [1,3,5,7,9]
-  print sort.bubble_sort(test1_list)
+  print bubble_sort(test1_list)
   
-  print sort.insert_sort(test1_list)
+  print insert_sort(test1_list)
   
-  print sort.merge_sort(test1_list)
+  print merge_sort(test1_list)
   
-  print sort.quick_sort(test1_list)
+  print quick_sort(test1_list)
   
   test2_list = [9,7,5,4,1]
-  print sort.bubble_sort(test2_list)
+  print bubble_sort(test2_list)
   
-  print sort.insert_sort(test2_list)
+  print insert_sort(test2_list)
   
-  print sort.merge_sort(test2_list)
+  print merge_sort(test2_list)
   
-  print sort.quick_sort(test2_list)
+  # print sort.quick_sort(test2_list)
   
   test3_list = [11,15,86,2,4]
-  print sort.bubble_sort(test3_list)
+  print bubble_sort(test3_list)
   
-  print sort.insert_sort(test3_list)
+  print insert_sort(test3_list)
   
-  print sort.merge_sort(test3_list)
+  print merge_sort(test3_list)
   
-  print sort.quick_sort(test3_list)
+  # print sort.quick_sort(test3_list)
 main()
