aa09c866f033a8f83f4a2b9250d283c075a4fea7
weijie2 <jeremykid1992@gmail.com>
2017-04-24 17:11:47 -0600

    Add hello world cpp

---
+++ python_practice/helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//library for input and output
+
+int //expected return value
+main(int argc, const char **argv)//argc is the filename, argv represent the multiple arguments 
+    std::cout << "hi\n";
+
+    return 0;
+}
