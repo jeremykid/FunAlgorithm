cb78a3c972cbdc6d8816891cbcc1da612f25e383
weijie2 <jeremykid1992@gmail.com>
2018-07-10 13:58:14 -0600

    Add MLP_NeuralNetwork

--- python_practice/neuralNetwork.py
+++ python_practice/neuralNetwork.py
@@ -36,3 +36,71 @@ class NeuralNetwork:
         self.weights1 += d_weights1
         self.weights2 += d_weights2
         
+class MLP_NeuralNetwork:
+    def __init__(self, input, hidden, output, iterations, learning_rate, momentum, rate_decay):
+        """
+        :param input: number of input neurons
+        :param hidden: number of hidden neurons
+        :param output: number of output neurons
+        """
+        # initialize parameters
+        self.iterations = iterations
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.rate_decay = rate_decay
+        
+        # initialize arrays
+        self.input = input + 1 # add 1 for bias node
+        self.hidden = hidden
+        self.output = output
+
+        # set up array of 1s for activations
+        self.activeInput = [1.0] * self.input
+        self.activeHidden = [1.0] * self.hidden
+        self.activeOutput = [1.0] * self.output
+       
+ def feedForward(self, inputs):
+        """
+        The feedforward algorithm loops over all the nodes in the hidden layer and
+        adds together all the outputs from the input layer * their weights
+        the output of each node is the sigmoid function of the sum of all inputs
+        which is then passed on to the next layer.
+        :param inputs: input data
+        :return: updated activation output vector
+        """
+        if len(inputs) != self.input-1:
+            raise ValueError('Wrong number of inputs!')
+
+        # input activations
+        for i in range(self.input -1): # -1 is to avoid the bias
+            self.ai[i] = inputs[i]
+
+        # hidden activations
+        for j in range(self.hidden):
+            sum = 0.0
+            for i in range(self.input):
+                sum += self.ai[i] * self.wi[i][j]
+            self.ah[j] = tanh(sum)
+
+        # output activations
+        for k in range(self.output):
+            sum = 0.0
+            for j in range(self.hidden):
+                sum += self.ah[j] * self.wo[j][k]
+            self.ao[k] = sigmoid(sum)
+
+        return self.ao[:]        
+        # create randomized weights
+        # use scheme from 'efficient backprop to initialize weights
+        input_range = 1.0 / self.input ** (1/2)
+        output_range = 1.0 / self.hidden ** (1/2)
+        self.weightInput = np.random.normal(loc = 0, scale = input_range, size = (self.input, self.hidden))
+        self.weightOutput = np.random.normal(loc = 0, scale = output_range, size = (self.hidden, self.output))
+        
+        # create arrays of 0 for changes
+        # this is essentially an array of temporary values that gets updated at each iteration
+        # based on how much the weights need to change in the following iteration
+        self.changeInput = np.zeros((self.input, self.hidden))
+        self.changeOutput = np.zeros((self.hidden, self.output))
+        
+        
