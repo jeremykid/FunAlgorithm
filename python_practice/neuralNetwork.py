5ca105c7b22d25531ef4e00b394834e84d9d042d
weijie2 <jeremykid1992@gmail.com>
2018-07-10 14:45:55 -0600

    Add neural network

--- python_practice/neuralNetwork.py
+++ python_practice/neuralNetwork.py
@@ -120,3 +120,18 @@ class MLP_NeuralNetwork:
 
         
         
+if __name__ == "__main__":
+    x = np.array([[0,1,0],
+                  [0,1,1],
+                  [1,0,1],
+                  [1,1,1]])
+    y = np.array([[0],[1],[1],[0]])
+    
+    nn = NeuralNetwork(X,y)
+
+
+    for i in range(2000):
+        nn.feedforward()
+        nn.backprop()
+    
+    print(nn.output)
