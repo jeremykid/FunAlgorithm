def7e3aeaf3b0cd1a6486c72c68a3baad77ef3e5
weijie2 <jeremykid1992@gmail.com>
2019-02-13 10:46:47 -0700

    Create leetcode-50.py

---
+++ python_practice/leetCode/leetcode-50.py
@@ -0,0 +1,12 @@
+class Solution:
+    def myPow(self, x: 'float', n: 'int') -> 'float':
+        return x**n
+
+    def myPow2(self, x: 'float', n: 'int') -> 'float':
+        if n == 0:
+            return 1
+        if n < 0:
+            n = 0-n
+            x = 1/x
+        
+        return x**(n%2)*myPow2(x*x, n//2)
