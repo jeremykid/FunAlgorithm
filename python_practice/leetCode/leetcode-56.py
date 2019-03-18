e55e718277a65e7423f7b8b217604a4e22d69a36
weijie2 <jeremykid1992@gmail.com>
2019-03-18 14:39:42 -0600

    Create leetcode-56.py

---
+++ python_practice/leetCode/leetcode-56.py
@@ -0,0 +1,35 @@
+#Merge Intervals
+class Solution:
+    def merge(self, intervals: List[Interval]) -> List[Interval]:
+        #sortInterval
+        if (not len(intervals)):
+            return []
+        
+        startSortedIntervals = sorted(intervals, key=self.sortByStartFunction)
+        
+        startIndex = 0
+        start = 0
+        end = 0
+        results = []
+        while startIndex < len(intervals):
+            if (startSortedIntervals[startIndex].start > end):
+
+                if (startIndex != 0):
+                    results.append([start, end])
+
+                start = startSortedIntervals[startIndex].start
+                end = startSortedIntervals[startIndex].end
+            elif (startSortedIntervals[startIndex].end > end):
+                end = startSortedIntervals[startIndex].end
+
+            startIndex = startIndex + 1
+
+        if (startSortedIntervals[-1].end > end):
+            end = startSortedIntervals[-1].end
+        results.append([start, end])
+        
+        return results
+
+
+    def sortByStartFunction(self, interval):
+        return interval.start        
